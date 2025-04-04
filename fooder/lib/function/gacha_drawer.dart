028ae970e68afb6c46a48be31cfc763f1ffcddc2
liuh2so4 <xuanyu60413@gmail.com>
2025-04-05 03:40:58 +0800

    for demo; Todo: set up good algorithm

--- fooder/lib/function/gacha_drawer.dart
+++ fooder/lib/function/gacha_drawer.dart
@@ -1,10 +1,46 @@
 import 'dart:math';
 import 'package:fooder/function/define_restaurant.dart';
 
+// class GachaDrawer {
+//   final Random _random = Random();
+//   final List<Restaurant> gachaPool;
+//   final List<Restaurant> _drawnRestaurants = [];
+
+//   GachaDrawer({required this.gachaPool});
+
+//   Future<Map<String, String>> drawCard() async {
+//     if (gachaPool.isEmpty) {
+//       return {
+//         'name': "無可推薦的餐廳",
+//         'image': 'lib/assets/show.jpg',
+//       };
+//     }
+
+//     // 如果所有餐廳都已抽過，則重置已抽列表
+//     if (_drawnRestaurants.length == gachaPool.length) {
+//       _drawnRestaurants.clear();
+//     }
+
+//     Restaurant selectedRestaurant;
+//     do {
+//       int index = _random.nextInt(gachaPool.length);
+//       selectedRestaurant = gachaPool[index];
+//     } while (_drawnRestaurants.contains(selectedRestaurant));
+
+//     _drawnRestaurants.add(selectedRestaurant);
+
+//     return {
+//       'name': selectedRestaurant.name,
+//       'image': selectedRestaurant.photoUrls.isNotEmpty
+//           ? 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${selectedRestaurant.photoUrls[0]}&key=YOUR_API_KEY'
+//           : 'lib/assets/show.jpg',
+//     };
+//   }
+// }
+
 class GachaDrawer {
-  final Random _random = Random();
   final List<Restaurant> gachaPool;
-  final List<Restaurant> _drawnRestaurants = [];
+  int _currentIndex = 0;
 
   GachaDrawer({required this.gachaPool});
 
@@ -16,18 +52,11 @@ class GachaDrawer {
       };
     }
 
-    // 如果所有餐廳都已抽過，則重置已抽列表
-    if (_drawnRestaurants.length == gachaPool.length) {
-      _drawnRestaurants.clear();
-    }
+    // 根據目前 index 抽餐廳
+    Restaurant selectedRestaurant = gachaPool[_currentIndex];
 
-    Restaurant selectedRestaurant;
-    do {
-      int index = _random.nextInt(gachaPool.length);
-      selectedRestaurant = gachaPool[index];
-    } while (_drawnRestaurants.contains(selectedRestaurant));
-
-    _drawnRestaurants.add(selectedRestaurant);
+    // 更新 index，並做環狀處理
+    _currentIndex = (_currentIndex + 1) % gachaPool.length;
 
     return {
       'name': selectedRestaurant.name,
